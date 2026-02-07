bf62a7229500cfebbd84730b455f68ba97f6675d
harrypotter1501 <cz2678@columbia.edu>
2026-02-07 12:24:23 -0500

    remove debug

--- windows-client/src/pages/ChatPage.tsx
+++ windows-client/src/pages/ChatPage.tsx
@@ -1,6 +1,13 @@
+import { useRef, useState } from 'react'
 import { ChatInterface } from '../components/ChatInterface'
+import { ChatSession } from '@/components/ChatInterface'
 
 export function ChatPage() {
+    // chat histories
+    const [historyItems, setHistoryItems] = useState<ChatSession[]>([])
+    const nextSessionIdRef = useRef<number>(1)
+    const [activeChatId, setActiveChatId] = useState<number>(0)
+
     return (
         <div className="h-full w-full flex flex-col overflow-hidden">
             {/* <header className="flex-shrink-0 p-4 border-b border-slate-800 bg-slate-900/50 backdrop-blur">
@@ -8,7 +15,12 @@ export function ChatPage() {
                 <p className="text-xs text-slate-400">Powered by Ollama and Weaviate.</p>
             </header> */}
             <div className="flex-1 min-h-0 flex flex-col overflow-hidden">
-                <ChatInterface />
+                <ChatInterface
+                    historyItems={historyItems}
+                    setHistoryItems={setHistoryItems}
+                    nextSessionIdRef={nextSessionIdRef}
+                    activeChatId={activeChatId}
+                    setActiveChatId={setActiveChatId} />
             </div>
         </div>
     )
